f5ad0cc94eb3cc4fe611b81174e9358ee924e9de
Danil Semelenov <mail@danil.mobi>
2015-07-08 21:59:00 +0300

    Rename SCRIPT_DIR variable to base_dir

--- config.example.py
+++ config.example.py
@@ -1,9 +1,9 @@
 import os
 
-SCRIPT_DIR = os.path.join(os.path.dirname(__file__), '')
-DB_PATH = SCRIPT_DIR + "data.sqlite"
-URLS_PATH = SCRIPT_DIR + "urls"
-OUTPUT_DIR = SCRIPT_DIR + "www"
+base_dir = os.path.dirname(__file__) + '/'
+DB_PATH = base_dir + "data.sqlite"
+URLS_PATH = base_dir + "urls"
+OUTPUT_DIR = base_dir + "www"
 SOCKET_TIMEOUT = 30
 NUMBER_OF_PROCESSES = 10
 GROUP_INTERVAL = 60 * 60
